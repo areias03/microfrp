48051ef7639f6f6d37129b4acdedd63f033d2494
areias03 <alexandre@qut-lpa00161720.qut.edu.au>
2026-05-05 12:34:33 +1000

    add: score combination script

---
+++ workflow/scripts/combine_scores.py
@@ -0,0 +1,23 @@
+import argparse
+import polars as pl
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Combine classifications from multiple files."
+    )
+    parser.add_argument("files", nargs="+", help="Path to input classification files.")
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="Path to the output combined classification file.",
+    )
+    args = parser.parse_args()
+
+    dfs = [pl.read_csv(file) for file in args.files]
+    df_joined = dfs[0]
+    for df in dfs[1:]:
+        df_joined = df_joined.join(df, on="taxon").write_csv(
+            args.output, separator="\t"
+        )
